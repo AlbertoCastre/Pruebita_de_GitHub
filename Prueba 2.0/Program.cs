0c5da8fbc81b891ae05709104c9a01a01ed9c0b0
Dr.Gino <betotenoch@gmail.com>
2023-05-26 08:11:53 -0500

    Agregar archivos de proyecto.

---
+++ Prueba 2.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prueba_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("esTO ES UNA PRUEBA");
+        }
+    }
+}
